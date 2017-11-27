e85486dedb208b41958761ed46c0bf42979654e4
Marc André Freiheit <marcandre@freiheit.software>
2017-11-27 18:25:09 +0100

    Add first green test

---
+++ src/test/java/de/hpi/bpt/scylla/TravisTests.java
@@ -0,0 +1,14 @@
+package de.hpi.bpt.scylla;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TravisTests {
+    /**
+     * First test to validate that travis is working
+     */
+    @Test
+    public void TravisIsWorking() {
+        Assert.assertEquals(true, true);
+    }
+}
