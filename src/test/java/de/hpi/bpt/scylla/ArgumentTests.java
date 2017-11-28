d95eaa01077230ee04fcbab0512a11b6277e796b
Marc André Freiheit <marcandre@freiheit.software>
2017-11-28 13:30:02 +0100

    Add usage method and corresponding test

--- src/test/java/de/hpi/bpt/scylla/ArgumentTests.java
+++ src/test/java/de/hpi/bpt/scylla/ArgumentTests.java
@@ -1,10 +1,29 @@
 package de.hpi.bpt.scylla;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.*;
 
 public class ArgumentTests {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+    
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+        System.setErr(null);
+    }
+    
     /**
      * There has to be a config file.
      */
@@ -15,4 +34,33 @@ public class ArgumentTests {
 
         assertThrows(IllegalArgumentException.class, e);
     }
+
+    @Test
+    public void missingBpmnFile() {
+
+    }
+    
+    @Test
+    public void missingSimFile() {
+
+    }
+
+    @Test
+    public void outputPath() {
+
+    }
+
+    @Test
+    public void help() {
+        String[] args = { "--help" };
+        
+        try {
+			Scylla.main(args);
+		} catch (Exception e) {
+			throw new AssertionError("Help message does not working.");
+        }
+        
+        assertTrue(outContent.toString().contains("Usage:"), "Help method.");
+    }
+
 }
