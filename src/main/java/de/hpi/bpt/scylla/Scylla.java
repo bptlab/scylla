807745e962fe1d9e69703de7733d8f7e342a8a9e
Marc André Freiheit <marcandre@freiheit.software>
2017-11-28 11:36:13 +0100

    Add support to provide arguments instead of hard coding values

--- src/main/java/de/hpi/bpt/scylla/Scylla.java
+++ src/main/java/de/hpi/bpt/scylla/Scylla.java
@@ -1,16 +1,15 @@
 package de.hpi.bpt.scylla;
 
+import java.util.Arrays;
+
 /**
  * Scylla is an extensible simulator for business processes in BPMN. <br>
  * This class initializes and runs the {@link SimulationManager} which simulates the processes based on the given input.
- *
+ * 
  * @author Tsun Yin Wong
  *
  */
 public class Scylla {
-
-	public static final String OS = System.getProperty("os.name");
-    public static final String FILEDELIM = (OS.contains("Linux") || OS.contains("Mac OS")) ? "/" : "\\";
 
     public static void main(String[] args) {
         /**
@@ -21,80 +20,33 @@ public class Scylla {
          * Simulation scenarios to test plug-ins.
          */
 
-        String folder = "samples" + FILEDELIM;
-//      String resFilename = folder + "EmptyGC.xml";
-    	String resFilename = folder + "p0_globalconf.xml";
+        String configurationFile = args[0];
+        String[] bpmnFilenames = Arrays.stream(Arrays.copyOfRange(args, 1, args.length))
+                                                .filter(x -> x.contains(".bpmn"))
+                                                .toArray(String[]::new);
 
+        String[] simFilenames = Arrays.stream(Arrays.copyOfRange(args, 1, args.length))
+                                                .filter(x -> x.contains(".xml"))
+                                                .toArray(String[]::new);
 
-//        String[] bpmnFilename = new String[] { folder + "p1_boundary.bpmn", folder + "p2_normal.bpmn",
-//            folder + "p3_subproc.bpmn" };
-//        String[] simFilenames = new String[] { folder + "p1_boundary_sim.xml", folder + "p2_normal_sim.xml",
-//            folder + "p3_subproc_sim.xml" };
+        String outputFolder = args[args.length - 1];
 
-
-        /**
+    	/**
          * Simulation scenarios to test dmn simulation.
          */
-
-
-      String[] bpmnFilename = new String[] { folder + "p1_boundary.bpmn"};
-      String[] simFilenames = new String[] { folder + "p1_boundary_sim.xml"};
-
-//        String[] bpmnFilename = new String[] { folder + "old\\p2_normal.bpmn"};
-//        String[] simFilenames = new String[] { folder + "old\\p2_normal_sim.xml"};
-
-
-//         String[] bpmnFilename = new String[] { folder + "p4_parallel.bpmn" };
-//         String[] simFilenames = new String[] { folder + "p4_parallel_sim.xml" };
-
-
-//      String[] bpmnFilename = new String[] { folder + "p6_return.bpmn" };
-//      String[] simFilenames = new String[] { folder + "p6_return_sim.xml" };
-
-
-//        String[] bpmnFilename = new String[] { folder + "Projekt_Event_Based_Gateway.bpmn" };
-//        String[] simFilenames = new String[] { folder + "EmptySC.xml" };
-//        String[] bpmnFilename = new String[] { folder + "test.bpmn" };
-//        String[] simFilenames = new String[] { folder + "test.xml" };
-
-
-//      String[] bpmnFilename = new String[] { folder + "p8_normalbpmn_with_dataobject.bpmn"};
-//      String[] simFilenames = new String[] { folder + "p8_normalbpmn_with_dataobject_sim.xml"};
-
-
-//        String[] bpmnFilename = new String[] { folder + "p5_batch.bpmn", folder + "p6_return.bpmn" };
-//        String[] simFilenames = new String[] { folder + "p5_batch_sim.xml", folder + "p6_return_sim.xml" };
-//        String resFilename = folder + "p56_conf_thesis.xml";
-
+    	
         /**
          * Simulation scenarios to test batch processes. May be used for regular simulation.
          */
-
-
-//         String[] bpmnFilename = new String[] { folder + "p61_return_batch.bpmn" };
-//         String[] simFilenames = new String[] { folder + "p61_return_batch_sim.xml" };
-//         String resFilename = folder + "p61_conf_batch.xml";
-
-
-//         String folder = "E:\\desmoj_reports\\batch\\";
-//
-//         String[] bpmnFilename = new String[] { folder + "p5_batch.bpmn", folder + "p6_return.bpmn" };
-//         String[] simFilenames = new String[] { folder + "p5_batch_sim.xml", folder + "p6_return_sim.xml" };
-//         String resFilename = folder + "p56_conf_thesis.xml";
 
         /**
          * END of simulation scenarios
          */
 
-//        String[] bpmnFilename = new String[] { folder + "p61_return_batch.bpmn" };
-//        String[] simFilenames = new String[] { folder + "p61_return_batch_sim.xml" };
-//        String resFilename = folder + "p61_conf_batch.xml";
-
-
         boolean enableBpsLogging = true;
-        boolean enableDesmojLogging = false;
+        boolean enableDesmojLogging = true;
 
-        SimulationManager manager = new SimulationManager(folder, bpmnFilename, simFilenames, resFilename,
+        SimulationManager manager = new SimulationManager(outputFolder, bpmnFilenames, simFilenames, configurationFile,
                 enableBpsLogging, enableDesmojLogging);
         manager.run();
     }
