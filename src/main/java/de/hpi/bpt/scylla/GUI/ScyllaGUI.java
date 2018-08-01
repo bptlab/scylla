60d4a08f7d711c0c21f626c746e098b56c7d5d95
Leon Bein <leon.bein@student.hpi.de>
2018-08-01 10:23:08 +0200

    Set DEBUG flag to false

--- src/main/java/de/hpi/bpt/scylla/GUI/ScyllaGUI.java
+++ src/main/java/de/hpi/bpt/scylla/GUI/ScyllaGUI.java
@@ -39,7 +39,7 @@ import de.hpi.bpt.scylla.GUI.WebSwing.WebSwingUtils;
 public class ScyllaGUI extends JFrame {
 	
 	/**A Developer variable to suppress e.g. output forwarding*/
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 	
 	/**Tells whether the programm is running in a jar or not*/
 	public static final boolean INJAR = ScyllaGUI.class.getResource(ScyllaGUI.class.getSimpleName()+".class").toString().startsWith("jar:");
