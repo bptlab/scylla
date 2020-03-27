f50f643dc581362e4c8ca427c411559d8d871b90
Sven <sven.ladusch@web.de>
2020-03-27 18:55:13 +0100

    added task terminate plugin

---
+++ src/main/java/de/hpi/bpt/scylla/plugin/rembrandtConnector/FreeResourceAtTerminateEvent.java
@@ -0,0 +1,21 @@
+package de.hpi.bpt.scylla.plugin.rembrandtConnector;
+
+import de.hpi.bpt.scylla.exception.ScyllaRuntimeException;
+import de.hpi.bpt.scylla.plugin_type.simulation.event.TaskTerminateEventPluggable;
+import de.hpi.bpt.scylla.simulation.ProcessInstance;
+import de.hpi.bpt.scylla.simulation.event.TaskTerminateEvent;
+
+public class FreeResourceAtTerminateEvent extends TaskTerminateEventPluggable {
+
+    @Override
+    public String getName() {
+        return rembrandtConnectorUtils.PLUGIN_NAME;
+    }
+
+    @Override
+    public void eventRoutine(TaskTerminateEvent terminateEvent, ProcessInstance processInstance) throws ScyllaRuntimeException {
+        // TODO: free Resource in Rembrandt
+        System.out.println("free resource: " + resourceAssignmentPlugin.resourceTaskMap.get(Integer.toString(terminateEvent.getNodeId()) + "." + Integer.toString(terminateEvent.getProcessInstance().getId())));
+    }
+
+}
