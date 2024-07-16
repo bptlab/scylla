30f568671757fab02663f0de93c5649526b4824d
leonbein <leon.bein@tum.de>
2024-07-16 18:10:05 +0200

    Fix bug: Timerevents without definition throw errors

--- src/main/java/de/hpi/bpt/scylla/plugin/bpmntimer/BPMNTimerPluginUtils.java
+++ src/main/java/de/hpi/bpt/scylla/plugin/bpmntimer/BPMNTimerPluginUtils.java
@@ -24,7 +24,7 @@ class BPMNTimerPluginUtils {
         TimeSpan timeSpan = null;
         Map<EventDefinitionType, Map<String, String>> definitions = processModel.getEventDefinitions().get(nodeId);
         if (definitions != null) {
-            if (definitions.get(EventDefinitionType.TIMER) != null) {
+            if (definitions.get(EventDefinitionType.TIMER) != null) { // = If this is a timer event
                 Map<String, String> eventAttributes = definitions.get(EventDefinitionType.TIMER);
                 // String timeDate = eventAttributes.get("timeDate");
                 // if (timeDate != null) {
@@ -33,10 +33,13 @@ class BPMNTimerPluginUtils {
                 // }
                 // String timeCycle = eventAttributes.get("timeCycle");
                 String timeDuration = eventAttributes.get("timeDuration");
-                Duration jtDuration = Duration.parse(timeDuration);
-                long duration = jtDuration.getSeconds();
-                TimeUnit timeUnit = TimeUnit.SECONDS;
-                timeSpan = new TimeSpan(duration, timeUnit);
+                if (timeDuration != null) {
+                    Duration jtDuration = Duration.parse(timeDuration);
+                    long duration = jtDuration.getSeconds();
+                    TimeUnit timeUnit = TimeUnit.SECONDS;
+                    timeSpan = new TimeSpan(duration, timeUnit);
+                }
+                // TODO handle timer definition other than timeDuration (ProcessModelParser at least also knows timeDate and timeCycle)
             }
         }
         return timeSpan;
